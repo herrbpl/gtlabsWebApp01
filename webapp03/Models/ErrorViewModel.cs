7e85251e93ee79aae2bebb7daccc3c35330e1641
Siim Aus <siim@sotf.ee>
2017-12-27 14:12:28 +0200

    Add project files.

---
+++ webapp03/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace webapp03.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
